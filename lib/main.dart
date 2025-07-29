5a8686b0d6471fc40c1c2f80f1c983b26bc588c2
rawa.shafaay <rawa.shafaay@globant.com>
2025-07-30 00:05:49 +0300

    create app routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:habitu/core/app_routes.dart';
 
 void main() {
   runApp(const HabituApp());
@@ -9,6 +10,10 @@ class HabituApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppRoutes.splash,
+      onGenerateRoute: AppRoutes.onGenerateRoute,
+    );
   }
 }
