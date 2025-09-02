cfbd2b90360011182684cdcac26adea3c20db8e7
rawa.shafaay <rawa.shafaay@globant.com>
2025-09-02 19:02:02 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Habitu());
+}
+
+class Habitu extends StatelessWidget {
+  const Habitu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
